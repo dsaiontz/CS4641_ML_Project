e11ce2644e3dee22617c60f9fa33a1846d7bc2e2
sshook98 <sscheckmate1@gmail.com>
2020-11-16 17:11:41 -0500

    initial structure

---
+++ NeuralNetwork.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras;
+from tensorflow.keras import layers;
+from tensorflow.models import Sequential;
+
+
+IMAGE_DATASET_DIR = './NoFuelCapTrainingSet'
+
+OUTPUT_DATASET_DIR = './ClusteredNoFuelCapData'
+
+if __name__ == "__main__":
+    
+    #make directory for clustered data, should it not exist already
+    if not os.path.exists(OUTPUT_DATASET_DIR):
+        os.makedirs(OUTPUT_DATASET_DIR)
+
+    time_elapsed = [] #for metrics purpose, how long it took to cluster each image
+    for filename in os.listdir(IMAGE_DATASET_DIR):
+        start = time.perf_counter()
+        print(filename)
+        
+        #reads image in and flattens it into 2d matrix
+        img = imread(os.path.join(IMAGE_DATASET_DIR, filename))
